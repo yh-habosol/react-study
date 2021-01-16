d51c3770246143d7907bd735ee559b9a753ce8b3
yh-habosol <k0912h@naver.com>
2021-01-16 18:53:57 +0900

    vscode setting
    first react app

---
+++ do-it-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">do it! react</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
